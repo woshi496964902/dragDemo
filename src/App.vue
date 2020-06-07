b3c3f90b01728d057a1d2fa10706e52994afa6ab
lmc <demo@lmc.com>
2020-06-07 12:19:25 +0800

    可视化demo

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+   <draggable></draggable>
+  </div>
+</template>
+
+<script>
+import Draggable from './components/Draggable' 
+
+export default {
+  name: 'App',
+  components: {
+    Draggable
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
